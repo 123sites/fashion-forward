3d412c07c5196a0994cbf0b95ed46cf270ec7600
CF <cfreitas12345@gmail.com>
2023-07-23 23:44:50 -0700

    First commit with basic set-up.

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,13 @@
+// REDO THIS /////////////////////////
+
+import { render } from "@testing-library/react";
+import StudentList from "../components/studentList";
+import { StudentProvider } from "../utils/studentContext";
+
+test("StudentList renders", () => {
+  render(
+    <StudentProvider>
+      <StudentList />
+    </StudentProvider>
+  );
+});
